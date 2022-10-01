260fc8cfc455afdcb9319af810e2bd7e2afb7f78
MakkuSaiko <AdriaanBenn0@gmail.com>
2022-10-01 13:35:41 +0200

    Add project files.

---
+++ ConsoleApp2/Month.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    
+    class Month
+    {
+        static int maxDailyProduction = 200;        //Max production per day for each month
+        string monthName;
+        int monthDays;
+        int maxProduction;      //Maximum production per month = nr of days * 200
+        int totalProduction;
+
+
+        public Month(string name, int days)
+        {
+            monthName = name;
+            monthDays = days;
+            maxProduction = monthDays * maxDailyProduction;    //calculate the maximum to be produced this month based on days
+        }
+            
+        public override String ToString()
+        {
+            return String.Format("Month: {0}\tMonth days: {1}\tProduction: {2}", monthName, monthDays, totalProduction);
+        }
+
+        public void setTotalProduction(int production)
+        {
+            totalProduction = production;
+        }
+        public int getMaxProduction()
+        {
+            return maxProduction;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int annualProduction = 10000;           //Total annual production
+            int limitProduction = 1000;             //limit the max production for the month to 1000
+            
+            Month[] months = new Month[12];         //lists of months
+            populateMonthList(ref months);
+
+            Random random = new Random();
+
+            //get production for each month
+            foreach(Month month in months)
+            {
+                int production = random.Next(0, month.getMaxProduction()+1);     //get a random production for the month
+                if (production > limitProduction) { 
+                    production = limitProduction;
+                    production = random.Next(0, production);
+                }
+                if (annualProduction < production) production = annualProduction;
+                   
+                
+                if (annualProduction > production) annualProduction -= production;
+                month.setTotalProduction(production);
+             
+            }
+
+            
+
+            writeToFile(months);
+
+
+            
+        }
+
+        static void populateMonthList(ref Month[] months)
+        {
+            int nrEl = 0;
+            StreamReader sr = new StreamReader("Months.txt");
+            while(!sr.EndOfStream)
+            {
+                String[] cur = sr.ReadLine().Split(",");
+                String name = cur[0];
+                int days = int.Parse(cur[1]);
+                months[nrEl] = new Month(name, days);
+                nrEl++;
+            }
+            sr.Close();
+
+        }
+
+        static void writeToFile(Month[] months)
+        {
+            StreamWriter sw = new StreamWriter("Output.txt");
+            foreach(Month month in months)
+            {
+                sw.WriteLine(month.ToString());
+            }
+            sw.Close();
+        }
+
+        
+    }
+}
